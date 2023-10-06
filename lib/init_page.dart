814efa1ac13e230a879ac3e9fa09e6e2b23a661c
matiasdev30 <matiasdev30@gmail.com>
2023-10-06 10:47:27 +0100

    stable animation, I need to add the repositoy to remote

--- lib/init_page.dart
+++ lib/init_page.dart
@@ -13,14 +13,42 @@ class _HomeViewState extends State<HomeView>
     with SingleTickerProviderStateMixin {
   late AnimationController animationControllerText;
   late Animation moveTextAnimation;
+  bool expanded = false;
+
+  late AnimationController animationController;
+
+  List<EmailCardModel> emailsCard = [
+    EmailCardModel(name: "Olavio Pimpadas", color: const Color(0xff655D4C)),
+    EmailCardModel(name: "Mvni Null", color: const Color(0xff0D6644)),
+    EmailCardModel(name: "Smith Bambino", color: const Color(0xff8F9493)),
+    EmailCardModel(name: "Lismar Lima", color: const Color(0xffDD2EFF)),
+    EmailCardModel(name: "Giazi Monteiro", color: const Color(0xff316DD8)),
+    EmailCardModel(name: "Rubem Sousa PO", color: const Color(0xffFEAF13)),
+    EmailCardModel(name: "Cleuver Suares", color: const Color(0xff55EAFF)),
+  ];
+
+  late PageController pageController;
+
+  double _currentPage = 0.0;
+
+  bool expand = false;
+
+  void _coffeScrollListener() {
+    setState(() {
+      _currentPage = pageController.page!;
+    });
+  }
 
   @override
   void initState() {
+    pageController = PageController(viewportFraction: 0.35, initialPage: 2);
+    pageController.addListener(_coffeScrollListener);
+
     animationControllerText =
         AnimationController(vsync: this, duration: const Duration(seconds: 8))
           ..repeat();
     moveTextAnimation =
-        Tween<double>(begin: 600, end: -615).animate(animationControllerText);
+        Tween<double>(begin: 1040, end: -1040).animate(animationControllerText);
 
     super.initState();
   }
@@ -37,7 +65,9 @@ class _HomeViewState extends State<HomeView>
         body: Stack(
       children: [
         Container(
-          color:  Colors.grey,
+          color: _currentPage == emailsCard.length
+              ? emailsCard.last.color
+              : emailsCard[_currentPage.toInt()].color,
           width: screenWidth(context),
           height: screenHeigth(context),
           child: Column(
@@ -54,12 +84,16 @@ class _HomeViewState extends State<HomeView>
                                 child: */
                         Transform.scale(
                             alignment: Alignment.center,
-                            scale: 20 ,
-                            child: const Text(
-                              "MVNI NULL",
-                              style: TextStyle(
-                                  fontSize: 12, fontFamily: "MyFont2"),
-                            )),
+                            scale: 20,
+                            child: _currentPage == emailsCard.length
+                                ? const SizedBox.shrink()
+                                : Text(
+                                    emailsCard[_currentPage.toInt()]
+                                        .name
+                                        .toUpperCase(),
+                                    style: const TextStyle(
+                                        fontSize: 12, fontFamily: "MyFont2"),
+                                  )),
                   )),
                   builder: (context, child) {
                     return Transform.translate(
@@ -76,129 +110,141 @@ class _HomeViewState extends State<HomeView>
           padding: const EdgeInsets.only(left: 20, top: 80, right: 20),
           child: Positioned(
             child: Row(children: [
-               const CircleAvatar(
-                            radius: 24,
-                            backgroundColor: Colors.black,
-                            child: FlutterLogo(),
-                          ),
+              const CircleAvatar(
+                radius: 24,
+                backgroundColor: Colors.black,
+                child: FlutterLogo(),
+              ),
               const Spacer(),
               CircleAvatar(
                 radius: 22,
-                backgroundColor: Colors.grey.shade600,
+                backgroundColor: Colors.black.withOpacity(0.2),
                 child: IconButton(
                     onPressed: () {},
-                    icon:  Icon(
+                    icon: const Icon(
                       Icons.chat,
-                      color: Colors.grey[800],
+                      color: Colors.black,
                       size: 20,
                     )),
               ),
               const SizedBox(
                 width: 5,
               ),
-              
-               IconButton(
-                    onPressed: () {},
-                    icon:  Icon(
-                      Icons.inbox,
-                      color: Colors.grey[600],
-                      size: 24,
-                    )),
-                     const SizedBox(
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.inbox,
+                    color: Colors.black,
+                    size: 24,
+                  )),
+              const SizedBox(
                 width: 5,
               ),
-               IconButton(
-                    onPressed: () {},
-                    icon:  Icon(
-                      Icons.send_outlined,
-                      color: Colors.grey[600],
-                      size: 24,
-                    )),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.send_outlined,
+                    color: Colors.black,
+                    size: 24,
+                  )),
               const Spacer(),
-               IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.keyboard_arrow_down_rounded,
-                      color: Colors.black,
-                      size: 30,
-
-                    )),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.keyboard_arrow_down_rounded,
+                    color: Colors.black,
+                    size: 30,
+                  )),
             ]),
           ),
         ),
-        EmailCard(
-          opacity: 1,
-          margin: 16,
-        ),
-      ],
-    ));
-  }
+        AnimatedOpacity(
+            duration: const Duration(milliseconds: 200),
+            opacity: expanded ? 0 : 1,
+            child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    expanded = true;
+                  });
+                },
+                child: PageView.builder(
+                    scrollDirection: Axis.vertical,
+                    controller: pageController,
+                    itemCount: emailsCard.length + 1,
+                    itemBuilder: (context, index) {
+                      if (index == 0) {
+                        return const SizedBox.shrink();
+                      }
+                      final result = _currentPage - index + 1;
+                      final value = -.42 * result + 1;
+                      final opacity = value.clamp(0.0, 1.0);
 
-  mailCard(
-      {double buttom = 10,
-      double margin = 0,
-      double rotate = 0,
-      required double opacity}) {
-    EdgeInsets edgeInsets = EdgeInsets.symmetric(horizontal: margin / 2);
+                      print(result);
 
-    return AnimatedPositioned(
-      bottom: buttom,
-      duration: const Duration(milliseconds: 400),
-      child: GestureDetector(
-        onHorizontalDragDown: (details) {
-          print("play");
-          setState(() {
-            buttom = buttom - 8;
-            print(buttom);
-          });
-        },
-        child: Transform.rotate(
-          angle: rotate,
-          child: AnimatedOpacity(
-            duration: const Duration(milliseconds: 400),
-            opacity: 1,
-            child: Padding(
-              padding: edgeInsets,
-              child: Center(
-                child: Container(
-                  width: screenWidth(context) - margin,
-                  height: screenHeigth(context) / 2.15,
+                      return Transform.scale(
+                          alignment: Alignment.bottomCenter,
+                          scale: 1.6,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                bottom: 0, left: 80, right: 80),
+                            child: Transform(
+                              alignment: Alignment.bottomCenter,
+                              transform: Matrix4.identity()
+                                ..translate(
+                                    0.0,
+                                    index == _currentPage - 1
+                                        ? screenHeigth(context) /
+                                            8 *
+                                            (.99 - value).abs()
+                                        : screenHeigth(context) /
+                                            9 *
+                                            (.99 - value).abs())
+                                ..scale(value),
+                              child: Opacity(
+                                  opacity: _currentPage == emailsCard.length
+                                        ? 1 : opacity,
+                                  child: EmailCard(
+                                    colorsButton: _currentPage == emailsCard.length
+                                        ? emailsCard.last.color
+                                        : emailsCard[_currentPage.toInt()].color,
+                                    isCurrentPage: index == _currentPage + 1,
+                                    email: _currentPage == emailsCard.length
+                                        ? null
+                                        : emailsCard[_currentPage.toInt()],
+                                  )),
+                            ),
+                          ));
+                    }))),
+        IgnorePointer(
+          ignoring: !expanded,
+          child: GestureDetector(
+            onHorizontalDragDown: (value) {
+              setState(() {
+                if (value.localPosition.dy < 140) {
+                  expanded = false;
+                }
+              });
+            },
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 300),
+              opacity: !expanded ? 0 : 1,
+              child: Align(
+                alignment: const Alignment(0, .6),
+                child: AnimatedContainer(
                   decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(opacity),
-                    boxShadow: const <BoxShadow>[
-                      BoxShadow(
-                          color: Colors.grey,
-                          offset: Offset(1, 1),
-                          blurRadius: 1)
-                    ],
-                    borderRadius: BorderRadius.circular(20),
-                  ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(32)),
+                  alignment: Alignment.bottomCenter,
+                  height: expanded ? screenHeigth(context) : 330,
+                  duration: const Duration(milliseconds: 400),
+                  margin: const EdgeInsets.only(
+                      top: 80, left: 10, bottom: 10, right: 10),
                 ),
               ),
             ),
           ),
         ),
-      ),
-    );
-  }
-
-  item({required IconData icon, required String text}) {
-    return Row(
-      children: [
-        IconButton(
-            onPressed: () {
-              //animationController.reverse();
-            },
-            icon: Icon(
-              icon,
-              color: Colors.white,
-              size: 20,
-            )),
-        Text(
-          text,
-          style: const TextStyle(color: Colors.white, fontSize: 16),
-        )
       ],
-    );
+    ));
   }
 }
--- lib/page.dart
+++ lib/page.dart
@@ -2,17 +2,13 @@ import 'package:animated_mail_app/utils.dart';
 import 'package:flutter/material.dart';
 
 class EmailCard extends StatefulWidget {
-  double buttom;
-  double margin;
-  double rotate;
-  double opacity;
+  final bool isCurrentPage;
+  EmailCardModel? email;
+  final Color colorsButton;
 
-  EmailCard(
-      {super.key,
-      this.buttom = 90,
-      this.margin = 0,
-      this.rotate = 0,
-      required this.opacity});
+  EmailCard({
+    super.key, required this.isCurrentPage, this.email, required this.colorsButton,
+  });
 
   @override
   State<EmailCard> createState() => _EmailCardState();
@@ -20,229 +16,184 @@ class EmailCard extends StatefulWidget {
 
 class _EmailCardState extends State<EmailCard>
     with SingleTickerProviderStateMixin {
-  late AnimationController animationController;
-
-  List<EmailCardModel> emailsCard = [
-    EmailCardModel(buttom: 90),
-    EmailCardModel(buttom: 50),
-    EmailCardModel(buttom: 10),
-    EmailCardModel(buttom: 50),
-    EmailCardModel(buttom: 10),
-    EmailCardModel(buttom: 50),
-    EmailCardModel(buttom: 10)
-  ];
-
-  late PageController pageController;
-
-  double _currentPage = 0.0;
-
-  bool expand = false;
-
-  void _coffeScrollListener() {
-    setState(() {
-      _currentPage = pageController.page!;
-    });
-  }
-
-  @override
-  void initState() {
-    animationController = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 400));
-    pageController = PageController(viewportFraction: 0.35, initialPage: 2);
-    pageController.addListener(_coffeScrollListener);
-    super.initState();
-  }
+  
 
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      scrollDirection: Axis.vertical,
-      controller: pageController,
-      itemCount: emailsCard.length + 1,
-      itemBuilder: (context, index) {
-        if (index == 0) {
-          return const SizedBox.shrink();
-        }
-        final result = _currentPage - index + 1;
-        final value = -.42 * result + 1;
-        final opacity = value.clamp(0.0, 1.0);
-
-        print(result);
-
-        return Transform.scale(
-          alignment: Alignment.bottomCenter,
-          scale: 1.6,
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 0, left: 80, right: 80),
-            child: Transform(
-              alignment: Alignment.bottomCenter,
-              transform: Matrix4.identity()
-                ..translate(
-                    0.0,
-                    index == _currentPage - 1
-                        ? screenHeigth(context) / 8 * (.99 - value).abs()
-                        : screenHeigth(context) / 9 * (.99 - value).abs())
-                ..scale(value),
-              child: Opacity(
-                opacity: opacity,
-                child: Container(
-                  child: Column(
-                    children: [
-                      AnimatedContainer(
-                        duration: const Duration(milliseconds: 200),
-                        padding: const EdgeInsets.only(
-                          top: 4,
-                          left: 4,
-                          right: 4,
-                        ),
-                        margin: const EdgeInsets.only(bottom: 3, top: 3),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: const <BoxShadow>[
-                            BoxShadow(
-                              color: Colors.grey,
-                              offset: Offset(0, 0),
-                              blurRadius: 1,
-                            ),
-                          ],
-                          color: Colors.white,
-                        ),
-                        child: Center(
-                          child: Column(children: [
-                            Row(
-                              children: [
-                                const CircleAvatar(
-                                  radius: 16,
-                                ),
-                                const Spacer(),
-                                if (index == _currentPage + 1) ...[
-                                  const Spacer(),
-                                  CircleAvatar(
-                                    radius: 14,
-                                    backgroundColor: Colors.grey[200],
-                                    child: IconButton(
-                                        onPressed: () {},
-                                        icon: const Icon(
-                                          Icons.chat,
-                                          color: Colors.grey,
-                                          size: 13,
-                                        )),
-                                  ),
-                                  const SizedBox(
-                                    width: 5,
-                                  ),
-                                  CircleAvatar(
-                                    radius: 14,
-                                    backgroundColor: Colors.yellow[50],
-                                    child: IconButton(
-                                        onPressed: () {},
-                                        icon: const Icon(
-                                          Icons.star,
-                                          color: Colors.yellow,
-                                          size: 14,
-                                        )),
-                                  ),
-                                ] else ...[
-                                  const Text("11:00"),
-                                  const SizedBox(
-                                    width: 10,
-                                  )
-                                ]
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 40,
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.only(left: 4, right: 4),
-                              child: Row(
-                                children: [
-                                  Text(
-                                    "Jackson Houston",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16),
-                                  ),
-                                  Spacer(),
-                                  Text(
-                                    "10:30",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.only(left: 4, right: 4),
-                              child: Row(
-                                children: [
-                                  Text(
-                                    "The Number 1 Secret Of Sucess",
-                                    style: TextStyle(
-                                        fontSize: 11,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.grey),
-                                  ),
-                                  Spacer(),
-                                  Text(
-                                    "22.06.2022",
-                                    style: TextStyle(
-                                        fontSize: 11,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.grey),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 4,
-                            ),
-                            Container(
-                                width: screenWidth(context),
-                                height: 89,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                    color: Colors.grey[200])),
-                            const SizedBox(
-                              height: 4,
-                            ),
-                          ]),
-                        ),
-                      ),
-                      Container(
-                          width: screenWidth(context),
-                          height: 38,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.white)),
-                      const SizedBox(
-                        height: 3,
-                      ),
-                      index == _currentPage + 1
-                          ? Container(
-                              width: screenWidth(context),
-                              height: 10,
-                              decoration: const BoxDecoration(
-                                  borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(20),
-                                      topRight: Radius.circular(20)),
-                                  color: Colors.white))
-                          : const SizedBox.shrink(),
-                    ],
+    return widget.email == null ? Transform.scale(
+      scale: 1.6,
+      child: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            padding: const EdgeInsets.only(
+              top: 4,
+              left: 4,
+              right: 4,
+            ),
+            margin: const EdgeInsets.only(bottom: 3, top: 3),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: const <BoxShadow>[
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0, 0),
+                  blurRadius: 1,
+                ),
+              ],
+              color: Colors.white,
+            ),
+            child: const Center(
+              child: Text("Sem emails disponivel", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),))),
+    ) : Column(
+      children: [
+        AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          padding: const EdgeInsets.only(
+            top: 4,
+            left: 4,
+            right: 4,
+          ),
+          margin: const EdgeInsets.only(bottom: 3, top: 3),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: const <BoxShadow>[
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(0, 0),
+                blurRadius: 1,
+              ),
+            ],
+            color: Colors.white,
+          ),
+          child: Center(
+            child: Column(children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 16,
+                    backgroundColor: widget.colorsButton,
                   ),
+                  const Spacer(),
+                  if (widget.isCurrentPage) ...[
+                    const Spacer(),
+                    CircleAvatar(
+                      radius: 14,
+                      backgroundColor: widget.colorsButton.withOpacity(.2),
+                      child: IconButton(
+                          onPressed: () {},
+                          icon:  Icon(
+                            Icons.chat,
+                            color: widget.colorsButton,
+                            size: 13,
+                          )),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    CircleAvatar(
+                      radius: 14,
+                      backgroundColor: Colors.yellow[50],
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 14,
+                          )),
+                    ),
+                  ] else ...[
+                    const Text("11:00"),
+                    const SizedBox(
+                      width: 10,
+                    )
+                  ]
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+               Padding(
+                padding: const EdgeInsets.only(left: 4, right: 4),
+                child: Row(
+                  children: [
+                    Text(
+                      widget.email!.name,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    const Spacer(),
+                    const Text(
+                      "10:30",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                  ],
                 ),
               ),
-            ),
+              const Padding(
+                padding: EdgeInsets.only(left: 4, right: 4),
+                child: Row(
+                  children: [
+                    Text(
+                      "The Number 1 Secret Of Sucess",
+                      style: TextStyle(
+                          fontSize: 11,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    ),
+                    Spacer(),
+                    Text(
+                      "22.06.2022",
+                      style: TextStyle(
+                          fontSize: 11,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Container(
+                  width: screenWidth(context),
+                  height: 89,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.grey[200])),
+              const SizedBox(
+                height: 4,
+              ),
+            ]),
           ),
-        );
-      },
+        ),
+        Container(
+            width: screenWidth(context),
+            height: 38,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white)),
+        const SizedBox(
+          height: 3,
+        ),
+        widget.isCurrentPage
+            ? Container(
+                width: screenWidth(context),
+                height: 10,
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
+                    color: Colors.white))
+            : const SizedBox.shrink(),
+      ],
     );
   }
 }
 
 class EmailCardModel {
-  double buttom;
 
-  EmailCardModel({required this.buttom});
+  String name;
+  Color color;
+
+  EmailCardModel({required this.name, required this.color});
 }
